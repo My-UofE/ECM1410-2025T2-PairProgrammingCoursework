58c2c53136942fc83593817d970e4abb0bf1445b
or305 <or305@exeter.ac.uk>
2025-03-03 19:22:22 +0000

    Combine managine files

--- gamesleague/Manager.java
+++ gamesleague/Manager.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerManager {
+public class Manager {
     private List<Player> players = new ArrayList<>();
 
     public int[] getPlayerIds() {
